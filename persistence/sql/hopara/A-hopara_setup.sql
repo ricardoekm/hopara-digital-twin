4d596fd3a5a762d173621bfe665d7ffd44d2a09c
Ricardo Mayerhofer <ricardo@hopara.io>
2026-04-22 06:18:05 -0300

    fix: hopara user already created

--- persistence/sql/hopara/A-hopara_setup.sql
+++
@@ -1,5 +0,0 @@
--- The A prefix is important so the file runs before the migrations (postgres sort by file name)
-CREATE USER hopara WITH PASSWORD 'hopara';
-CREATE SCHEMA AUTHORIZATION hopara;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA hopara grant all on tables to hopara;
